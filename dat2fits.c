50b2077ba9ace5b577e17961f45ad5bc33552729
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-01-10 10:27:34 +0800

    Update dat2fits.c

--- dat2fits.c
+++ dat2fits.c
@@ -60,19 +60,19 @@ main(int argc, char *argv[]){            //argc(argument count) argv(argument ve
   }
 
   //--------------save header---------------------------------
-  fprintf(fitsfp,"SIMPLE  =                    T /%48s"," ");
-  fprintf(fitsfp,"BITPIX  =                   32 /%48s"," ");
-  fprintf(fitsfp,"NAXIS   =                    2 /%48s"," ");     
-  fprintf(fitsfp,"NAXIS1  =               %05d  /%48s",lnum," ");     
-  fprintf(fitsfp,"NAXIS2  =                 %03d  /%48s",bnum," "); 
-  fprintf(fitsfp,"BSCALE  = %+1.13e /%48s",0.001," ");   
+  fprintf(fitsfp,"SIMPLE  =                    T /%48s"," ");   //指明文件是否符合FITS标准，%48表示48个空字符数
+  fprintf(fitsfp,"BITPIX  =                   32 /%48s"," ");   //每一个像元值的存储位数
+  fprintf(fitsfp,"NAXIS   =                    2 /%48s"," ");   //指明图像里的坐标轴数     
+  fprintf(fitsfp,"NAXIS1  =               %05d  /%48s",lnum," ");   //X(RA)方向像元数     
+  fprintf(fitsfp,"NAXIS2  =                 %03d  /%48s",bnum," ");   //Y(Dec)方向像元数，输出一个数值变量，不足3位在前面补0
+  fprintf(fitsfp,"BSCALE  = %+1.13e /%48s",0.001," ");   //用于将像元值转换为真实值的参数：真实值=(磁带值×BSCALE)＋BZERO 
   fprintf(fitsfp,"EXTEND  =                    T /%48s"," ");   
   fprintf(fitsfp,"DATE    =   '2019-10-23'       /%48s"," ");  
-  fprintf(fitsfp,"CTYPE1  = 'GLON-GLS'           /%48s"," ");  
-  fprintf(fitsfp,"CRVAL1  = %+1.13e /%48s", 120.0," ");
-  fprintf(fitsfp,"CDELT1  = %+1.13e /%48s",-1.0/60.0," ");
-  fprintf(fitsfp,"CRPIX1  = %+1.13e /%48s",6226.0," ");
-  fprintf(fitsfp,"CROTA1  =                    0 /%48s"," ");
+  fprintf(fitsfp,"CTYPE1  = 'GLON-GLS'           /%48s"," ");   //RA=CRVAL1+(i-CRPIX1)*CDELT1/COS(Dec)
+  fprintf(fitsfp,"CRVAL1  = %+1.13e /%48s", 120.0," ");   //X轴参考位置
+  fprintf(fitsfp,"CDELT1  = %+1.13e /%48s",-1.0/60.0," ");   //X轴坐标增量
+  fprintf(fitsfp,"CRPIX1  = %+1.13e /%48s",6226.0," ");   //X轴像素位置
+  fprintf(fitsfp,"CROTA1  =                    0 /%48s"," ");   //
   fprintf(fitsfp,"CTYPE2  = 'GLAT-GLS'           /%48s"," ");  
   fprintf(fitsfp,"CRVAL2  = %+1.13e /%48s",0.0," ");    
   fprintf(fitsfp,"CDELT2  = %+1.13e /%48s",1.0/60.0," ");
